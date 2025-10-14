80e852a70f78e49bf0ce2ce40d97558401d24fc3
João Barros <joaopilzbarros@gmail.com>
2025-10-13 23:52:09 -0300

    carregando páginas internas

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php require "../app/fuctions/pages.php";?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP - UDEMY</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+            require load();
+        ?>
+    </div>
+</body>
+</html>
